69ceb510b35a3ecf432d64a494e58f6e4ac66d31
Max <uue33@mail.ru>
2019-10-20 22:59:02 +0500

    Почти слайдер, без крашенных крадратов, без движения только в право

--- js/jquery.js
+++ js/jquery.js
@@ -1,2 +1,45 @@
+var slideNow = 1;
+var slideCount = $('.slidewrapper').children().length;
+var interval = 2000;
+var navBtnId = 0;
+var translateWidth = 0;
 
- 
+$(document).ready(function() {
+    var switchInterval = setInterval(nextSlide, interval);
+
+    $('.viewport').hover(function() {
+        clearInterval(switchInterval);
+    }, function() {
+        switchInterval = setInterval(nextSlide, interval);
+    });
+
+
+    $('.slide-nav-btn').click(function() {
+        navBtnId = $(this).index();
+
+        if (navBtnId + 1 != slideNow) {
+            translateWidth = -$('.viewport').width() * (navBtnId);
+            $('.slidewrapper').css({
+                'transform': 'translate(' + translateWidth + 'px, 0)',
+                '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+                '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+            });
+            slideNow = navBtnId + 1;
+        }
+    });
+});
+
+function nextSlide() {
+    if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
+        $('.slidewrapper').css('transform', 'translate(0, 0)');
+        slideNow = 1;
+    } else {
+        translateWidth = -$('.viewport').width() * (slideNow);
+        $('.slidewrapper').css({
+            'transform': 'translate(' + translateWidth + 'px, 0)',
+            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+        });
+        slideNow++;
+    }
+}
